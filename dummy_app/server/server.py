15c60d890a6c38426e9cd27af973b8352218891a
oz <ozfraier@gmail.com>
2018-05-11 02:18:35 +0300

    MONGOLAB_URI -> MONGODB_URI

--- dummy_app/server/server.py
+++ dummy_app/server/server.py
@@ -12,7 +12,7 @@ app = Flask(__name__, static_folder='../static/dist', template_folder='../static
 #app.config["MONGO_DBNAME"] = "tsatetota_db"
 mongo = PyMongo(app, config_prefix='MONGO')
 #APP_URL = "http:#127.0.0.1:5000"
-uri = os.environ.get('MONGOLAB_URI')
+uri = os.environ.get('MONGODB_URI')
 #if not uri:
 #    MONGO_URL = "mongodb:#localhost:27017/rest";
 client = MongoClient(uri)
